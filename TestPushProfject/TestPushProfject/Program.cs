ba5d36b7c7080cf47e34cb36e2dda34077a55051
kamolsingh <31326302+kamolsingh@users.noreply.github.com>
2017-08-25 13:54:35 -0700

    Mar is better

--- TestPushProfject/TestPushProfject/Program.cs
+++ TestPushProfject/TestPushProfject/Program.cs
@@ -7,7 +7,7 @@ namespace TestPushProfject
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Earth!");
+            Console.WriteLine("Hello Mar!");
         }
     }
 }
