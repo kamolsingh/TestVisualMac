1bdec25aa3543eda1b3f5b938489bb3c98ac330c
kamolsingh <31326302+kamolsingh@users.noreply.github.com>
2017-08-25 14:06:24 -0700

    Revert "Merge branch 'master' of https://github.com/kamolsingh/TestVisualMac"
    This reverts commit 66a488a9f4464f5e6d4f4e9ce33dea94931287ee, reversing
    changes made to ba5d36b7c7080cf47e34cb36e2dda34077a55051.

--- TestPushProfject/TestPushProfject/Program.cs
+++ TestPushProfject/TestPushProfject/Program.cs
@@ -7,11 +7,7 @@ namespace TestPushProfject
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
             Console.WriteLine("Hello Mar!");
-=======
-            Console.WriteLine("Hello Jupiter!");
->>>>>>> fc6d39fe4e2be97e133bb905fa660e12e8e28cec
         }
     }
 }
